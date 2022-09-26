ee9cb19d3ad1ee926091c2b19f370997381e393a
Abhinav gupta <105590510+abhinav20062001@users.noreply.github.com>
2022-09-26 11:42:13 +0530

    Add files via upload

---
+++ js/questionset.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "HTML is considered as ______ ?",
+    answer: "Markup language",
+    options: [
+      "High level language",
+      "Markup language",
+      "Programming language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "HTML was first proposed in ___.",
+    answer: "1990",
+    options: [
+      "1993",
+      "1985",
+      "1980",
+      "1990"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HTML uses ______?",
+    answer: "Fixed tags defined by the language",
+    options: [
+      "Predefined tags",
+      "User-defined tags",
+      "Tags for links only",
+      "Fixed tags defined by the language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the main author of the HTML?",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Brendan Eich",
+      "Tim Berners-Lee",
+      "Le Narmes",
+      "Kim Saa"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The HTML standard which does not require double quotes around the values of an attribute is called ______?",
+    answer: "HTML 5",
+    options: [
+      "HTML 3",
+      "HTML 7",
+      "HTML 5",
+      "HTML 4"
+    ]
+  },
+    {
+    numb: 6,
+    question: "The property in CSS used to change the background color of an element is -",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "backcolor",
+      "bgcolor",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which organization defines Web standards?",
+    answer: "World Wide Web Consortium",
+    options: [
+      "IBM Corporation",
+      "Apple Inc.",
+      "Microsoft Corporation",
+      "World Wide Web Consortium"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which of the following is the correct syntax to make the background-color of all paragraph elements to yellow?",
+    answer: "p {background-color : yellow;}",
+    options: [
+      "p {background-color : #yellow;}",
+      "all {background-color : yellow;}",
+      "all p{background-color : yellow;}",
+      "p {background-color : yellow;}"
+    ]
+  },
+    {
+    numb: 9,
+    question: " Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      "JavaScript is a High-level language",
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
